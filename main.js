13da84ef1383a0d1cbe73adf77df43ebb8413694
abderrahmenmankai <57589717+abderrahmenmankai@users.noreply.github.com>
2020-05-02 22:38:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// //function toggle() {}
+// window.addEventListener('resize', function() {
+// 	let element = document.getElementById('callId');
+// 	let width = window.innerWidth;
+// 	if (element) {
+// 		if (width < 992) document.getElementById('callId').className = 'mb-phone';
+// 		else document.getElementById('callId').className = 'phone-call';
+// 	}
+// });
+
+// window.addEventListener('load', function() {
+// 	let element = document.getElementById('callId');
+// 	let width = window.innerWidth;
+// 	if (element) {
+// 		if (width < 992) document.getElementById('callId').className = 'mb-phone';
+// 		else document.getElementById('callId').className = 'phone-call';
+// 	}
+// });
